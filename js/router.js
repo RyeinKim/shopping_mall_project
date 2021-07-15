b0ea8f00009688fd5b78b7a77b57c2ed1860762e
_ Project:None <reign6496@gmail.com>
2021-07-15 01:56:24 +0000

    -

---
+++ js/router.js
@@ -0,0 +1,11 @@
+router.post('/register', (req,res,next) =>{
+	console.log(req.body)
+    res.end()
+})
+
+const db = require('../db/config')
+
+router.post('/register', (req,res,next) =>{
+	console.log(req.body)
+    res.end()
+})
